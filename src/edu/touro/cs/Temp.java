dc69f0f2cffdeb8cfe74e5fa1d892ab478fbb482
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-03-09 15:29:45 -0500

    Initial commit

---
+++ src/edu/touro/cs/Temp.java
@@ -0,0 +1,34 @@
+package edu.touro.cs;
+
+// TODO : Replace this file with your code
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
+class MyThread extends Thread
+{
+    public static List<Integer> list =new ArrayList<>();
+    @Override
+    public void run()
+    {
+        for (int i =0;i<10_000_000;i++)
+            list.add(i);
+    }
+}
+
+public class Temp {
+    public static void main(String[] args) throws InterruptedException {
+        MyThread t1 = new MyThread(),
+               t2 = new MyThread();
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println( MyThread.list.size());
+    }
+}
